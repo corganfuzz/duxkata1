026b133632cf61712eec3e2ed0bf4af0b15188ce
corganfuzz <gruizcavero@uh.edu>
2018-03-25 15:24:53 -0500

    basic components

---
+++ src/actions/postActions.js
@@ -0,0 +1,24 @@
+import {FETCH_POSTS, NEW_POSTS} from './types';
+
+// export function fetchPosts () {
+//   return function (dispatch) {
+//     fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then(res => res.json())
+//       .then(posts =>
+//       dispatch({
+//         type: FETCH_POSTS,
+//         payload: posts
+//       })
+//     );
+// }}
+
+export const fetchPosts = () => dispatch => {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(res => res.json())
+    .then(posts => {
+      dispatch({
+        type: FETCH_POSTS,
+        payload: posts
+      })
+    })
+}
---
+++ src/components/Posts.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Posts extends Component {
+  render () {
+
+    return (
+      <div>
+        <h1>Posts</h1>
+        {/* {postItems} */}
+      </div>
+      );
+    }
+  }
+
+export default Posts;
